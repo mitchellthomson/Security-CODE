84f2f3e4a1a0f585a501c189c45a8197a1819dda
MitchellThomson <mitchell.thomson@code.berlin>
2022-04-30 20:17:56 +0200

    Added login and sign up

--- noteTaker/auth.py
+++ noteTaker/auth.py
@@ -1,16 +1,18 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET','POST'])
 def login():
+    data = request.form
     return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
     return "<p>Logout</p>"
 
-@auth.route('/signup')
+@auth.route('/signup', methods=['GET','POST'])
 def signup():
+    data = request.form
     return render_template("signup.html")
 
