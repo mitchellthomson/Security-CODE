60cd9daf5417d87e9c629fea45c0248ca5665b81
MitchellThomson <mitchell.thomson@code.berlin>
2022-05-01 18:03:40 +0200

    restricted login

--- noteTaker/auth.py
+++ noteTaker/auth.py
@@ -1,21 +1,23 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
-from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user
+from . import db
 
 
-auth = Blueprint('auth', __name__)
+auth = Blueprint("auth", __name__)
 
-@auth.route('/login', methods=['GET','POST'])
+@auth.route("/login", methods=["GET","POST"])
 def login():
-    if request.method == 'POST':
-        email = request.form.get('email')
-        psw = request.form.get('psw')
+    if request.method == "POST":
+        email = request.form.get("email")
+        psw = request.form.get("psw")
     
         user = User.query.filter_by(email=email).first()
         if user:
             if check_password_hash(user.psw, psw):
                 flash('Logged in', category='success')
+                login_user(user, remember=True)
             else:
                 flash('Incorrect password, try again!', category='error')
         else:
@@ -25,12 +27,12 @@ def login():
 
 
 
-@auth.route('/signup', methods=['GET','POST'])
+@auth.route("/signup", methods=["GET","POST"])
 def signup():
-    if request.method == 'POST':
-        email = request.form.get('email')
-        psw = request.form.get('psw')
-        pswRepeat = request.form.get('pswRepeat')
+    if request.method == "POST":
+        email = request.form.get("email")
+        psw = request.form.get("psw")
+        pswRepeat = request.form.get("pswRepeat")
         
         user = User.query.filter_by(email=email).first()
         if user:
@@ -42,14 +44,16 @@ def signup():
         elif psw != pswRepeat:
             flash('Your passwords do not match', category='error')
         else:
-            new_user = User(email=email, psw=generate_password_hash(psw, method='sha256'))
+            new_user = User(email=email, psw=generate_password_hash(psw, method="sha256"))
             db.session.add(new_user)
             db.session.commit()
-            flash('Account Created!', category='success')
+            flash('Account Created!', category="success")
             return redirect(url_for("views.home"))
         
     return render_template("signup.html")
 
 @auth.route("/logout")
+@login_required
 def logout():
-    return '<p>Logout</p>'
+    logout_user()
+    return redirect(url_for("auth.login"))
