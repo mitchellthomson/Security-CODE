330278b2fb1b7d4160527e3a0c4758c00720115d
MitchellThomson <mitchell.thomson@code.berlin>
2022-05-01 16:42:45 +0200

    Added User creation DB

--- noteTaker/auth.py
+++ noteTaker/auth.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 auth = Blueprint('auth', __name__)
 
@@ -13,6 +17,23 @@ def logout():
 
 @auth.route('/signup', methods=['GET','POST'])
 def signup():
-    data = request.form
+    if request.method == 'POST':
+        email = request.form.get('email')
+        psw = request.form.get('psw')
+        pswRepeat = request.form.get('pswRepeat')
+        
+        if len(email) < 4:
+            flash('Email must be longer than 4 characters', category='error')
+        elif len(psw) < 8:
+            flash('Password must be 8 or more characters', category='error')
+        elif psw != pswRepeat:
+            flash('Your passwords do not match', category='error')
+        else:
+            new_user = User(email=email, psw=generate_password_hash(psw, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            flash("Account Created!", category='success')
+            return redirect(url_for("views.home"))
+        
     return render_template("signup.html")
 
