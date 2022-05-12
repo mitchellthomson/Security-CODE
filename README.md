edc3e3348f1486f1d59257c938d805a32ec34a2c
MitchellThomson <mitchell.thomson@code.berlin>
2022-05-12 16:16:39 +0200

    Added complexity to db

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Code-Spring-2022
-My Webproject for code semester 2022
+My Webproject for code Spring 2022
+
+To run the application run the main.py python file and visit the localhost link
+
+
+
+
--- noteTaker/models.py
+++ noteTaker/models.py
@@ -10,10 +10,22 @@ class Note(db.Model):
     text = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    is_public = db.Column(db.Boolean)
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
     email = db.Column(db.String(150), unique = True)
     psw = db.Column(db.String(150))
-    
     notes = db.relationship("Note")
+    
+class Rating(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    score = db.Column(db.Integer)
+    user = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+class Edit(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    text = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    node_id = db.Column(db.Integer, db.ForeignKey("note.id"))
